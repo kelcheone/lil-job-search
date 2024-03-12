ff74be4547df0a7972dd114bca28ad3ac86ef0a2
kelche <kelcheone@gmail.com>
2024-03-12 22:16:12 +0300

    faet: welcome page

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+
+import { icons, SIZES, COLORS } from "../../../constants";
+
+const jobTypesData = ["Full Time", "Part Time", "Remote", "Freelance"];
+const Welcome = () => {
+  const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState(jobTypesData[0]);
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Kevin, who is your morning</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job.</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="Search for jobs"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            style={styles.searchBtnImage}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypesData}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
