51b34f1dec38f6dd34bc1448715fb814a5d64cad
kelche <kelcheone@gmail.com>
2024-03-14 13:22:20 +0300

    refactor: change the navigation button name

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -3,18 +3,18 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./nearbyjobcard.style";
 import { checkImageURL } from "../../../../utils";
 
-const NearbyJobCard = ({ job, selectedJob, handleNavige }) => {
+const NearbyJobCard = ({ job, handleNavigate }) => {
   const defaultImage =
     "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg";
   return (
     job && (
       <TouchableOpacity
         style={styles.container}
-        onPress={() => handleNavige(job)}
+        onPress={() => handleNavigate(job)}
       >
         <TouchableOpacity style={styles.logoContainer}>
           <Image
-            source={{ uri: defaultImage }}
+            source={{ uri: job.employer_logo || defaultImage }}
             resizeMode="contain"
             style={styles.logoImage}
           />
@@ -25,9 +25,7 @@ const NearbyJobCard = ({ job, selectedJob, handleNavige }) => {
             {job.job_title}
           </Text>
           <View style={styles.infoWrapper}>
-            <Text style={styles.publisher(selectedJob, job)}>
-              {job?.job_publisher} -
-            </Text>
+            <Text style={styles.publisher}>{job?.job_publisher} -</Text>
             <Text style={styles.jobType}> {job.job_employment_type}</Text>
           </View>
         </View>
