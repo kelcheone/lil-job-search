efca223c11e1231ee04d3817981c9e0badcb82d2
kelche <kelcheone@gmail.com>
2024-03-14 13:20:32 +0300

    feat: jobdetails page components

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 
-import styles from './tabs.style'
+import styles from "./tabs.style";
 
-const Tabs = () => {
+import { SIZES } from "../../../constants";
+
+const TabButton = ({ name, activeTab, setActiveTab }) => {
+  const onHandleSearchType = (name) => {
+    setActiveTab(name);
+  };
+
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      onPress={() => onHandleSearchType(name)}
+      style={styles.btn(activeTab, name)}
+    >
+      <Text style={styles.btnText(activeTab, name)}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default Tabs
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            name={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{
+          columnGap: SIZES.small / 2,
+        }}
+      />
+    </View>
+  );
+};
+
+export default Tabs;
